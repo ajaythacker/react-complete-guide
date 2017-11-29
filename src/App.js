34d43834a6bf26daf29b5e8d3715b27b99fddb5e
Ajay Thacker <athacker@healthgrades.com>
2017-11-29 12:56:14 -0700

    trying out setState

--- src/App.js
+++ src/App.js
@@ -4,13 +4,20 @@ import Person from './Person/Person';
 
 class App extends Component {
   state = {
-    persons: [{name: 'Max', age: 28}, {name: 'Manu', age: 29}, {name: 'Steph', age: 25}]
+    persons: [{name: 'Max', age: 28}, {name: 'Manu', age: 29}, {name: 'Steph', age: 25}],
+    otherState: 'other'
+  };
+
+  clickHandler = () => {
+    this.setState({
+      persons: [{name: 'Max', age: 28}, {name: 'Manuaa', age: 29}, {name: 'Steph', age: 26}]
+    });
   };
 
   render() {
     return (
       <div className="App">
-        <button>Switch Name</button>
+        <button onClick={this.clickHandler}>Switch Name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>
           My hobby is racing
