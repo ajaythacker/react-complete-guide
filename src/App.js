5717e6dae9155283d5a294e6f0e5ca0c0b1bc033
Ajay Thacker <athacker@healthgrades.com>
2017-11-30 13:27:34 -0700

    added show hide persons handler

--- src/App.js
+++ src/App.js
@@ -10,19 +10,56 @@ class App extends Component {
 
   clickHandler = () => {
     this.setState({
-      persons: [{name: 'Max', age: 28}, {name: 'Manuaa', age: 29}, {name: 'Steph', age: 26}]
+      persons: [{name: 'Max', age: 28}, {name: 'Manuaa', age: 29}, {name: 'Steph', age: 26}],
+      showPersons: false
+    });
+  };
+
+  changeHandler = event => {
+    this.setState({
+      persons: [
+        {name: event.target.value, age: 28},
+        {name: 'Manuaa', age: 29},
+        {name: 'Steph', age: 26}
+      ]
+    });
+  };
+
+  showHidePersons = () => {
+    const currentStatus = this.state.showPersons;
+    const newStatus = !currentStatus;
+    this.setState({
+      showPersons: newStatus
     });
   };
 
   render() {
+    let persons = null;
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          <Person
+            name={this.state.persons[0].name}
+            age={this.state.persons[0].age}
+            click={this.clickHandler}
+            change={this.changeHandler}
+          />
+          <Person
+            name={this.state.persons[1].name}
+            age={this.state.persons[1].age}
+            click={this.clickHandler}>
+            My hobby is racing
+          </Person>
+          <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+        </div>
+      );
+    }
+
     return (
       <div className="App">
         <button onClick={this.clickHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>
-          My hobby is racing
-        </Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+        <button onClick={this.showHidePersons}>Show/Hide</button>
+        {persons}
       </div>
     );
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,11 +2,14 @@ import React from 'react';
 
 const person = props => {
   return (
-    <div>
+    <div onClick={props.click}>
       <p>
         My name is {props.name} and I am {props.age} years old
       </p>
       <p>{props.children}</p>
+      <p>
+        <input type="text" onChange={props.change} value={props.name} />
+      </p>
     </div>
   );
 };
